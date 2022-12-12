bf7ee60c25434499f735d5367452e29cc3ba705a
MALEKOBEIDAT <115452586+MALEKOBEIDAT@users.noreply.github.com>
2022-12-12 04:29:52 -0800

    tasks

---
+++ 12-12/SQLQuery3.sql
@@ -0,0 +1,72 @@
+--create database task ;
+--create Table customer(
+--id int,
+--fname varchar(20),
+--lname varchar(20),
+
+
+
+--);
+
+--insert into customer(id,fname,lname)
+--values(2020901011,'Malek','Obeidat');
+--select * from customer;
+
+-----------------------------------------------------------
+
+
+
+
+--create Table orders(
+--Oid int,
+--Oname varchar(20),
+
+
+
+
+--);
+
+--insert into orders(Oid,Oname)
+--values(123456,'Mohammad');
+--select * from orders
+
+
+
+
+
+--create Table product(
+--pname varchar(20),
+--ptype varchar(20),
+--pprise int,
+
+
+
+
+--);
+
+--insert into product(pname,ptype,pprise)
+--values('Jeanz','turky',25);
+--select * from product
+
+
+
+
+create Table employee(
+ename varchar(20),
+eid int,
+ebirth int,
+
+
+
+
+);
+
+insert into employee(ename,eid,ebirth)
+values('Malek','14',2001);
+select * from employee
+
+
+
+
+
+
